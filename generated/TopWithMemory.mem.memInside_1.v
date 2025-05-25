5b206252a42b465149874be9f27c074d96fa6b28
cyx0173 <1908443836@qq.con>
2025-05-25 22:44:53 +0800

    转v

---
+++ generated/TopWithMemory.mem.memInside_1.v
@@ -0,0 +1,20 @@
+module BindsTo_1_mem(
+  input         clock,
+  input         reset,
+  input         io_reset,
+  input  [63:0] io_if_mem_instAddr,
+  input  [63:0] io_ex_mem_dataAddr,
+  input         io_ex_mem_writeEn,
+  input  [31:0] io_ex_mem_writeData,
+  input  [2:0]  io_ex_mem_func3,
+  output [31:0] io_mem_id_inst_0,
+  output [31:0] io_mem_id_inst_1,
+  output [31:0] io_mem_lsu_data
+);
+
+initial begin
+  $readmemh("G:\\testdata\\drystone_1.data", mem.memInside_1);
+end
+                      endmodule
+
+bind mem BindsTo_1_mem BindsTo_1_mem_Inst(.*);
---
+++ generated/firrtl_black_box_resource_files.f
@@ -0,0 +1,4 @@
+G:\CPU大作业\idea\dodo_rv32i\generated\TopWithMemory.mem.memInside_3.v
+G:\CPU大作业\idea\dodo_rv32i\generated\TopWithMemory.mem.memInside_2.v
+G:\CPU大作业\idea\dodo_rv32i\generated\TopWithMemory.mem.memInside_1.v
+G:\CPU大作业\idea\dodo_rv32i\generated\TopWithMemory.mem.memInside_0.v
