e5fc964deb654ad9663a971fc38dda099ce0f9d6
Zeyu Cao <22301010059@m.fudan.edu.cn>
2025-10-17 17:36:55 +0800

    Revert "Detached2"

--- generated/firrtl_black_box_resource_files.f
+++
@@ -1,4 +0,0 @@
-C:\Users\Lenovo\Desktop\Code\dodo_rv32i\generated\TopWithMemory.mem.memInside_3.v
-C:\Users\Lenovo\Desktop\Code\dodo_rv32i\generated\TopWithMemory.mem.memInside_2.v
-C:\Users\Lenovo\Desktop\Code\dodo_rv32i\generated\TopWithMemory.mem.memInside_1.v
-C:\Users\Lenovo\Desktop\Code\dodo_rv32i\generated\TopWithMemory.mem.memInside_0.v
